4456b3e991103caf43e1d4a72b4d7a015f3b955a
Valérie Pineau Noël <valerie.pineau-noel.1@ulaval.ca>
2022-07-20 11:41:54 -0400

    testing merging two images side by side with pillow

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import os
 import fnmatch
+from PIL import Image
 
 def listNameOfFiles(directory: str, extension="tif") -> list:
 	"""
@@ -25,3 +26,24 @@ def read_file(file_path):
 	"""
 	image_array = tiff.imread(file_path)
 	return image_array
+
+def merge_images(file1, file2):
+    """Merge two images into one, displayed side by side
+    :param file1: path to first image file
+    :param file2: path to second image file
+    :return: the merged Image object
+    """
+    image1 = Image.open(file1)
+    image2 = Image.open(file2)
+
+    (width1, height1) = image1.size
+    (width2, height2) = image2.size
+
+    result_width = width1 + width2
+    result_height = max(height1, height2)
+
+    result = Image.new('RGB', (result_width, result_height))
+    result.paste(im=image1, box=(0, 0))
+    result.paste(im=image2, box=(width1, 0))
+    
+    return result
--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+from functions import *
+
 sourcePath = "/Users/valeriepineaunoel/Desktop/test"
-files = 
+files = listNameOfFiles(directory=sourcePath)
+tileDimensions = [2, 2]
+
+pathFile1 = sourcePath + "/" + files[0]
+pathFile2 = sourcePath + "/" + files[1]
+
+stitchedImage = merge_images(file1=pathFile1, file2=pathFile2)
+stitchedImage.save(fp="stitchedImage.tif")
