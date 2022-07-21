ef86a66afbe9f499e5f1800de2e29745b7ac6401
Valérie Pineau Noël <valerie.pineau-noel.1@ulaval.ca>
2022-07-21 16:49:55 -0400

    Testing cross-phase correlation with homemade images
    Turns out that it works! So I think it's just not possible with two different images with an overlap because maybe of the noise that is different and the intensity that might vary between the two same structures in both images.

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ manualShift = [384, 39] # [y,x]. Positive y = going down. Positive x = going rig
 # initialize images in numpy arrays
 imagenp0 = read_file(filePath=sourcePath + "/" + files[0], imageType="numpy")
 imagenp1 = read_file(filePath=sourcePath + "/" + files[1], imageType="numpy")
-imagenp2 = read_file(filePath=sourcePath + "/" + files[2], imageType="numpy")
-imagenp3 = read_file(filePath=sourcePath + "/" + files[3], imageType="numpy")
+image_offset = read_file(filePath=sourcePath + "/" + files[2], imageType="numpy")
+#imagenp2 = read_file(filePath=sourcePath + "/" + files[2], imageType="numpy")
+#imagenp3 = read_file(filePath=sourcePath + "/" + files[3], imageType="numpy")
 
 # initialise images as pillow images
 imagePIL0 = read_file(filePath=sourcePath + "/" + files[0], imageType="PIL")
@@ -18,6 +19,6 @@ imagePIL2 = read_file(filePath=sourcePath + "/" + files[2], imageType="PIL")
 imagePIL3 = read_file(filePath=sourcePath + "/" + files[3], imageType="PIL")
 
 # calculate shift with cross-phase correlation does not give the right answer. 
-#coordinatesOfShift = calculate_shift(image1, image2)
+#coordinatesOfShift = calculate_shift(imagenp1, image_offset)
 
 #stitchedImage.save(fp="stitchedImage.tif")
