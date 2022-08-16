7396a74f3b94e4bf7ce272128eb183c9206ead5b
Valérie Pineau Noël <40174887+Valeriouus@users.noreply.github.com>
2022-08-16 08:13:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from stitching import *
 from imageTreatment import *
 import tifffile as tiff
 
-sourcePath = "/Users/valeriepineaunoel/Desktop/20220621-334A-1-Cryoprotectant-Ave30-tiling-zoom2/LineCorrection"
-tileDimensions = [229, 81]
+sourcePath = "/Users/valeriepineaunoel/Desktop/20220621-334A-1-Cryoprotectant-Ave30-tiling-zoom2/LineCorrection/IntensityCorrection"
+tileDimensions = [229, 2]
 
-stitch = Stitching(sourceDir=sourcePath, tileD=tileDimensions, imageSize=[1024,512], isIntensityCorrection=True, isMirrored=True, isFlipped=False)
+stitch = Stitching(sourceDir=sourcePath, tileD=tileDimensions, imageSize=[1024,512], isIntensityCorrection=False, isMirrored=True, isFlipped=False)
