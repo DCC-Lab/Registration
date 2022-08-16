7b8eb60d3940c4cb59435d09b6c49342e976b531
Valérie Pineau Noël <40174887+Valeriouus@users.noreply.github.com>
2022-08-16 09:38:53 -0400

    Testing the code for verification of its stitching
    Seems that the code calculates the shift with the stage position in the file name very well, but it is the way the files are named that confuses the stitching. The code assumes that the files are numbered in order, which is true, but the code also considers that 10 000 is smaller than 1 000 because it has more zeros. lol
    I had to rename the files to make it work, but this way is definitely something I need to change eventually.

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from imageTreatment import *
 import tifffile as tiff
 
 sourcePath = "/Users/valeriepineaunoel/Desktop/20220621-334A-1-Cryoprotectant-Ave30-tiling-zoom2/LineCorrection/IntensityCorrection"
-tileDimensions = [229, 2]
+tileDimensions = [229, 81]
 
 stitch = Stitching(sourceDir=sourcePath, tileD=tileDimensions, imageSize=[1024,512], isIntensityCorrection=False, isMirrored=True, isFlipped=False)
